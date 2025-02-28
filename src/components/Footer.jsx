eee1ce33476447da77a34b43d760bbed5e6e5332
Wes <wesley.nimmo@lauruscollege.edu>
2025-02-27 22:59:50 -0800

    Header/footer cosmetic adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+  
   return (
-    <div className='bg-primary text-accent'>
-      <h2 className='oswald text-center'>Footer</h2>
+    <div className='bg-primary text-accent py-3'>
+      <h4 className='oswald text-center'>All Rights Reserved &copy; 2025</h4>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-primary text-accent'>
-      <h2 className='text-center oswald'>Header</h2>
+    <div className='bg-primary text-accent py-4'>
+      <h2 className='text-center oswald text-2xl'>Laurus College</h2>
     </div>
   )
 }
